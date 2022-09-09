7e5b0f82e74d437981cf8eb0716fd4ec6baecf67
guang <guang@369.com>
2022-09-09 09:59:15 +0800

    first commit

---
+++ src/main/java/com/atguang/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguang.git;
+
+/**
+ * @author guang
+ * @date 2022/9/9 - 9:48
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("Hello Git!");
+    }
+}
